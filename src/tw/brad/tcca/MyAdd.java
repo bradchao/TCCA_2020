cdc706ffd6bae63928f0860c4ecc9b05c0b06041
user <user@PC323-96>
2020-06-14 13:26:58 +0800

    06141326

--- src/tw/brad/tcca/MyAdd.java
+++ src/tw/brad/tcca/MyAdd.java
@@ -6,10 +6,12 @@ public class MyAdd {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.print("X = ");
 		int x = scanner.nextInt();
+		System.out.print("Y = ");
 		int y = scanner.nextInt();
 		int result = x + y;
-		System.out.println(result);
+		System.out.printf("%d + %d = %d", x, y, result);
 	}
 
 }
