8eeb6918d1c7ce26d4ba033e085fec45db3ffb49
user <user@PC323-96>
2020-06-14 15:05:33 +0800

    06141505

--- src/tw/brad/tcca/Score.java
+++ src/tw/brad/tcca/Score.java
@@ -3,13 +3,17 @@ package tw.brad.tcca;
 public class Score {
 
 	public static void main(String[] args) {
-		int score = 97;
-		if (score >= 60) {
-			System.out.println("Pass");
-		}else {
-			System.out.println("Down");
-		}
-		System.out.println("End");
+
+		double doubleScore = Math.random();
+		System.out.println(doubleScore);
+		
+//		int score = 97;
+//		if (score >= 60) {
+//			System.out.println("Pass");
+//		}else {
+//			System.out.println("Down");
+//		}
+//		System.out.println("End");
 	}
 
 }
