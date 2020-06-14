dcbd606271d75ce217d4399e05aa4615f3320e6b
user <user@PC323-96>
2020-06-14 16:31:06 +0800

    06141631

--- src/tw/brad/tcca/LeapYear.java
+++ src/tw/brad/tcca/LeapYear.java
@@ -5,21 +5,30 @@ import java.util.Scanner;
 public class LeapYear {
 
 	public static void main(String[] args) {
+		boolean isLeap;
 		Scanner scanner = new Scanner(System.in);
 		int year = scanner.nextInt();
 		if (year % 4 == 0) {
 			if (year % 100 == 0) {
 				if (year % 400 == 0) {
 					// 29
+					isLeap = true;
 				}else {
 					// 28
+					isLeap = false;
 				}
 			}else {
 				// 29
+				isLeap = true;
 			}
 		}else {
 			// 28
+			isLeap = false;
 		}
+		
+		System.out.printf("%d年是%s年", year, isLeap?"閏":"平");
+		
+		
 		
 		/*
 		if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
