58d41a30e1e6b624619cf73c038f0d54e850eb1c
user <user@PC323-96>
2020-06-14 13:13:02 +0800

    202006141312

---
+++ src/tw/brad/tcca/MyAdd.java
@@ -0,0 +1,12 @@
+package tw.brad.tcca;
+
+public class MyAdd {
+
+	public static void main(String[] args) {
+		int x = 12;	// [a-zA-Z$_][a-zA-Z0-9$_]*
+		int y = 13;	// [a-zA-Z$_][a-zA-Z0-9$_]*
+		int result = x + y;
+		System.out.println(result);
+	}
+
+}
