0c05b0ce0e7444bfd69612fb14e28151e014f17f
user <user@PC323-96>
2020-06-14 13:22:03 +0800

    06141322

--- src/tw/brad/tcca/MyAdd.java
+++ src/tw/brad/tcca/MyAdd.java
@@ -1,10 +1,13 @@
 package tw.brad.tcca;
 
+import java.util.Scanner;
+
 public class MyAdd {
 
 	public static void main(String[] args) {
-		int x = 12;	// [a-zA-Z$_][a-zA-Z0-9$_]*
-		int y = 13;	// [a-zA-Z$_][a-zA-Z0-9$_]*
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
 		int result = x + y;
 		System.out.println(result);
 	}
