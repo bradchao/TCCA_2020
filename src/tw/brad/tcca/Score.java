0dc405256dcbf9b5dde13746c6a56ef86de08e1b
user <user@PC323-96>
2020-06-14 14:54:42 +0800

    06141454

---
+++ src/tw/brad/tcca/Score.java
@@ -0,0 +1,13 @@
+package tw.brad.tcca;
+
+public class Score {
+
+	public static void main(String[] args) {
+		int score = 57;
+		if (score >= 60) {
+			System.out.println("Pass");
+		}
+		System.out.println("End");
+	}
+
+}
