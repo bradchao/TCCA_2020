d356271004a24bb3373fba9d1521d271a57228fd
user <user@PC323-96>
2020-06-14 15:45:28 +0800

    06141545

--- src/tw/brad/tcca/ScoreV2.java
+++ src/tw/brad/tcca/ScoreV2.java
@@ -7,23 +7,21 @@ public class ScoreV2 {
 		double doubleScore = Math.random();
 		int score = (int)(doubleScore * 101);	// 
 		System.out.println(score);
+		
 		if (score >= 90) {
 			System.out.println("A");
+		}else if (score >= 80) {
+			System.out.println("B");
+		}else if (score >= 70) {
+			System.out.println("C");
+		}else if (score >= 60) {
+			System.out.println("D");
 		}else {
-			if (score >= 80) {
-				System.out.println("B");
-			}else {
-				if (score >= 70) {
-					System.out.println("C");
-				}else {
-					if (score >= 60) {
-						System.out.println("D");
-					}else {
-						System.out.println("E");
-					}
-				}
-			}
+			System.out.println("E");
 		}
+		
+		
+		
 		System.out.println("End");
 	}
 
