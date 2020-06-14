8f04a5dd9ad64101a98becd972c936031302282a
user <user@PC323-96>
2020-06-14 13:55:18 +0800

    06141355

---
+++ src/tw/brad/tcca/MyDivV2.java
@@ -0,0 +1,14 @@
+package tw.brad.tcca;
+
+import java.util.Scanner;
+
+public class MyDivV2 {
+
+	public static void main(String[] args) {
+		double x = 10;
+		double y = 3;
+		double r = x / y;
+		System.out.println(r);
+	}
+
+}
