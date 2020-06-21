d5343b0ab8d03969c2dbdb301f065822f242c7b5
user <user@PC323-96>
2020-06-21 09:40:47 +0800

    06210940

--- src/tw/brad/tcca/MonthDays.java
+++ src/tw/brad/tcca/MonthDays.java
@@ -5,12 +5,16 @@ public class MonthDays {
 	public static void main(String[] args) {
 		int month = (int)(Math.random()*12)+1, days = -1;
 		
-		if (month == 1 || month == 3) {
+		if (month == 1 || month == 3 || month == 5 || month == 7
+				 || month == 8 || month == 10 || month == 12) {
 			days = 31;
+		}else if(month == 4 || month == 6 
+				|| month == 9 || month == 11) {
+			days = 30;
 		}else if(month == 2) {
 			days = 28;
-		}else if(month == 4) {
-			days = 30;
+		}else {
+			days = 99;
 		}
 		
 		System.out.printf("%d月有%d天", month, days);
