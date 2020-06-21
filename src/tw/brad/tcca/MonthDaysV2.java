6e57a7265990fd1edc61317a140f530174aec9fb
user <user@PC323-96>
2020-06-21 09:58:52 +0800

    06210958

--- src/tw/brad/tcca/MonthDaysV2.java
+++ src/tw/brad/tcca/MonthDaysV2.java
@@ -3,20 +3,18 @@ package tw.brad.tcca;
 public class MonthDaysV2 {
 
 	public static void main(String[] args) {
-		int month = (int)(Math.random()*12)+1, days = -1;
+		int month = (int)(Math.random()*12)+1; 
+		int days = -1;
 
 		switch (month) {
-			case 1:
+			case 1: case 3: case 5: case 7: case 8: case 10: case 12:
 				days = 31;
+				break;
+			case 4: case 6: case 9: case 11:
+				days = 30;
 				break;
 			case 2:
 				days = 28;
-				break;
-			case 3:
-				days = 31;
-				break;
-			case 4:
-				days = 30;
 				break;
 			default:
 				days = 99;
