e9b72bb388938d5d9d054910c1b8fee8dfdabbf4
user <user@PC323-96>
2020-06-21 11:13:49 +0800

    06211113

---
+++ src/tw/brad/tcca/MyCounterV2.java
@@ -0,0 +1,19 @@
+package tw.brad.tcca;
+
+public class MyCounterV2 {
+
+	public static void main(String[] args) {
+		
+		for (int i=0; i<10; i++); {
+			System.out.println("Hello, World");
+		}
+		
+		int a = 11;
+		if (a<10);{
+			System.out.println("OK");
+		}
+		
+		
+	}
+
+}
---
+++ src/tw/brad/tcca/Nine9.java
@@ -0,0 +1,12 @@
+package tw.brad.tcca;
+
+public class Nine9 {
+
+	public static void main(String[] args) {
+		for (int i=2; i<=5; i++) {
+			int r = i * 1;
+			System.out.printf("%d x 1 = %d\t", i, r);
+		}
+	}
+
+}
