89a8903bdbd4f4fcac0034f908be65ae526e1c0a
user <user@PC323-96>
2020-06-21 10:22:29 +0800

    06211022

---
+++ src/tw/brad/tcca/HelloWorldV3.java
@@ -0,0 +1,13 @@
+package tw.brad.tcca;
+
+public class HelloWorldV3 {
+
+	public static void main(String[] args) {
+		int i = 1;
+		for (System.out.println("Brad"); i<=10 ;System.out.println("-----")) {
+			System.out.println("Hello, World");
+			i++;
+		}
+	}
+
+}
