7fa096b3aa8a8a41ce35e5d19ef248ff1bf6f363
user <user@PC323-96>
2020-06-21 11:49:07 +0800

    06211149

--- src/tw/brad/tcca/Nine9.java
+++ src/tw/brad/tcca/Nine9.java
@@ -3,10 +3,14 @@ package tw.brad.tcca;
 public class Nine9 {
 
 	public static void main(String[] args) {
-		for (int k=0; k<2; k++) {
+		int row = 3;
+		int start = 1;
+		int col = 3;
+		
+		for (int k=0; k<row; k++) {
 			for (int j=1; j<=9; j++) {
-				for (int i=2; i<=5; i++) {
-					int newi = i + k * 4;
+				for (int i=start; i<=(col+1); i++) {
+					int newi = i + k * col;
 					int r = newi * j;
 					System.out.printf("%d x %d = %d\t", newi, j, r);
 				}
