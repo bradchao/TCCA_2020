6369ef52a7acca66939f304d5a41c6ca91af194b
user <user@PC323-96>
2020-06-21 13:28:26 +0800

    06211328

---
+++ src/tw/brad/tcca/CubeV2.java
@@ -0,0 +1,18 @@
+package tw.brad.tcca;
+
+public class CubeV2 {
+
+	public static void main(String[] args) {
+		int[] p = new int[7];
+		for (int i = 0; i < 100; i++) {
+			int point = (int) (Math.random() * 6) + 1;	// 1 - 6
+			p[point]++;
+		}
+		
+		for (int i=1; i<=6; i++) {
+			System.out.printf("%d點出現%d次\n", i, p[i]);
+		}
+		
+	}
+
+}
