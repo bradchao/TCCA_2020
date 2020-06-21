8bba18ea3476e2ca67904a340af3153c9c16bd00
user <user@PC323-96>
2020-06-21 10:38:25 +0800

    06211038

---
+++ src/tw/brad/tcca/MyCount.java
@@ -0,0 +1,14 @@
+package tw.brad.tcca;
+
+public class MyCount {
+
+	public static void main(String[] args) {
+		int i=1;
+		for (;; ) {
+			System.out.println(i++);
+		}
+		//System.out.println("----");
+		//System.out.println(i);
+	}
+
+}
