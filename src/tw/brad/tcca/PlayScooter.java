efe7bc2bde49ce9b51247eb2d309f1b574020b55
user <user@PC323-96>
2020-07-05 14:24:15 +0800

    07051424

---
+++ src/tw/brad/tcca/PlayScooter.java
@@ -0,0 +1,14 @@
+package tw.brad.tcca;
+
+import tw.brad.utils.Scooter;
+
+public class PlayScooter {
+
+	public static void main(String[] args) {
+		Scooter s1 = new Scooter();
+		s1.upSpeed();
+		s1.upSpeed();
+		System.out.println(s1.getSpeed());
+	}
+
+}
