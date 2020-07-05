01e959234952822f17f9016fd0495e4ee7a8ae3d
user <user@PC323-96>
2020-07-05 11:17:58 +0800

    07051117

--- src/tw/brad/tcca/PlayBikeV2.java
+++ src/tw/brad/tcca/PlayBikeV2.java
@@ -7,12 +7,10 @@ public class PlayBikeV2 {
 	public static void main(String[] args) {
 		Bike myBike;
 		myBike = new Bike();
-		System.out.println(myBike.speed);
-		myBike.upSpeed();
-		myBike.upSpeed();
-		myBike.upSpeed();
-		myBike.upSpeed();
-		System.out.println(myBike.speed);
+		System.out.printf("%s's bike = %f\n", myBike.getName(), myBike.getSpeed());
+		myBike.setName("Brad");
+		for (int i=0; i<14; i++) myBike.upSpeed();
+		System.out.printf("%s's bike = %f\n", myBike.getName(), myBike.getSpeed());
 		
 	}
 
--- src/tw/brad/utils/Bike.java
+++ src/tw/brad/utils/Bike.java
@@ -1,5 +1,29 @@
 package tw.brad.utils;
 
 public class Bike {
-	public double speed;
+	private double speed;
+	private String name;
+	
+	public Bike() {
+		name = "nobody";
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getSpeed() {
+		return speed;
+	}
+	
+	public void upSpeed() {
+		speed = speed < 1 ? 1 : speed*1.2;
+	}
+	public void downSpeed() {
+		speed = speed < 1 ? 0 : speed*0.7;
+	}
 }
