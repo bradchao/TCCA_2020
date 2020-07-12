cb99abd5f88ae6d3882409bc6cfebdf515f9e80f
user <user@PC323-96>
2020-07-12 14:16:09 +0800

    07121416

--- src/tw/brad/utils/TWId.java
+++ src/tw/brad/utils/TWId.java
@@ -5,10 +5,21 @@ public class TWId {
 	
 	public static boolean isRightID(String id) {
 		boolean isRight = false;
-		if (id.length() == 10) {
-			if (id.charAt(1) == '1' || id.charAt(1) == '2') {
-				
-			}
+		String letters = "ABCDEFGHJKLMNPQRSTUVXYWZIO";
+		if (id.matches("[A-Z][12][0-9]{8}")) {
+			char c1 = id.charAt(0);
+			int n12 = letters.indexOf(c1)+10;
+			int n1 = n12 / 10;
+			int n2 = n12 % 10;
+			int n3 = Integer.parseInt(id.substring(1, 2));
+			int n4 = Integer.parseInt(id.substring(2, 3));
+			int n5 = Integer.parseInt(id.substring(3, 4));
+			int n6 = Integer.parseInt(id.substring(4, 5));
+			int n7 = Integer.parseInt(id.substring(5, 6));
+			int n8 = Integer.parseInt(id.substring(6, 7));
+			int n9 = Integer.parseInt(id.substring(7, 8));
+			int n10 = Integer.parseInt(id.substring(8, 9));
+			int n11 = Integer.parseInt(id.substring(9, 10));
 		}
 		return isRight;
 	}
