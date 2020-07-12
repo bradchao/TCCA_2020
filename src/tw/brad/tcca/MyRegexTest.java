3d343ba730f6d426ccedbeeee7644fe02a37fc17
user <user@PC323-96>
2020-07-12 14:25:35 +0800

    07121425

--- src/tw/brad/tcca/MyRegexTest.java
+++ src/tw/brad/tcca/MyRegexTest.java
@@ -1,14 +1,23 @@
 package tw.brad.tcca;
 
+import tw.brad.utils.TWId;
+
 public class MyRegexTest {
 
 	public static void main(String[] args) {
-		String a = "A12345678";
+		String a = "A123456789";
 		if (a.matches("[A-Z][12][0-9]{8}")) {
 			System.out.println("OK");
 		}else {
 			System.out.println("XX");
 		}
+		
+		if(TWId.isRightID("B123460300")) {
+			System.out.println("OK");
+		}else {
+			System.out.println("XX");
+		}
+		
 	}
 
 }
