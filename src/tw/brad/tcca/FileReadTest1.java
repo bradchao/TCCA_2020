797753cd24207748847b4ffb15187e14b2679745
user <user@PC323-96>
2020-07-19 11:58:04 +0800

    07191158

--- src/tw/brad/tcca/FileReadTest1.java
+++ src/tw/brad/tcca/FileReadTest1.java
@@ -9,7 +9,14 @@ public class FileReadTest1 {
 	public static void main(String[] args) {
 		try {
 			FileInputStream fin = new FileInputStream("dir1/file3.txt");
+
+			int temp;
+			while ( (temp = fin.read()) != -1) {
+				System.out.print((char)temp);
+			}
+			
 			fin.close();
+			//System.out.println("OK");
 		}catch(Exception e) {
 			System.out.println(e.toString());
 		}
