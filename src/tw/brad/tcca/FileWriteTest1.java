a28530a9ba53ba5d315ed62f487a17fc8334b2fc
user <user@PC323-96>
2020-07-19 13:49:06 +0800

    07191349

--- src/tw/brad/tcca/FileWriteTest1.java
+++ src/tw/brad/tcca/FileWriteTest1.java
@@ -5,9 +5,14 @@ import java.io.FileOutputStream;
 public class FileWriteTest1 {
 
 	public static void main(String[] args) {
+		String txt = "Hello, World\n1234567\n7654321";
+		String txt2 = "電腦公會";
 		try {
 			FileOutputStream fout = new FileOutputStream("dir1/file4.txt");
+			fout.write(txt2.getBytes());
+			fout.flush();
 			fout.close();
+			System.out.println("OK");
 		}catch(Exception e) {
 			System.out.println(e.toString());
 		}
