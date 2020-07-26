191c4e292f21b2f7411a64ce5997fe74b1f37dce
user <user@PC323-96>
2020-07-26 12:02:37 +0800

    07261200

--- src/tw/brad/tcca/TCPClient.java
+++ src/tw/brad/tcca/TCPClient.java
@@ -1,6 +1,9 @@
 package tw.brad.tcca;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -11,16 +14,20 @@ public class TCPClient {
 		try {
 			Socket socket = new Socket(
 					InetAddress.getByName("10.30.3.96"), 7777);
-			BufferedReader reader = 
-					new BufferedReader(
-							new InputStreamReader(
-									socket.getInputStream()));
-			String line;
-			while ( (line = reader.readLine()) != null) {
-				System.out.println(line);
+			BufferedInputStream bin = new BufferedInputStream(
+					socket.getInputStream());
+			BufferedOutputStream bout = new BufferedOutputStream(
+					new FileOutputStream("dir1/brad.jpg"));
+			byte[] buf = new byte[4*1024]; int len;
+			while ( (len = bin.read(buf)) != -1) {
+				bout.write(buf, 0, len);
 			}
-			reader.close();
+			bout.flush();
+			bout.close();
+			
+			bin.close();
 			socket.close();
+			System.out.println("OK");
 		}catch (Exception e) {
 			System.out.println(e.toString());
 		}
