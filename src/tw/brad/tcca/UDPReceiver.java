3d96cfb0cac27a2eb5958f9d595d137d270a1f42
user <user@PC323-96>
2020-07-26 10:54:27 +0800

    07261054

--- src/tw/brad/tcca/UDPReceiver.java
+++ src/tw/brad/tcca/UDPReceiver.java
@@ -6,16 +6,24 @@ import java.net.DatagramSocket;
 public class UDPReceiver {
 
 	public static void main(String[] args) {
-		byte[] buf = new byte[1024];
-		try {
-			DatagramSocket socket = new DatagramSocket(8888);
-			DatagramPacket packet = new DatagramPacket(buf, buf.length);
-			socket.receive(packet);
-			socket.close();
-			String urip = packet.getAddress().getHostAddress();
-			System.out.println("Receive OK:" + urip);
-		}catch(Exception e) {
-			System.out.println(e.toString());
+		while (true) {
+			byte[] buf = new byte[1024];
+			try {
+				DatagramSocket socket = new DatagramSocket(8888);
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				socket.receive(packet);
+				socket.close();
+				String urip = packet.getAddress().getHostAddress();
+				byte[] row = packet.getData();
+				int len = packet.getLength();
+				String mesg = new String(row,0,len);
+				System.out.println(urip + ":" + mesg);
+				if (mesg.equals("exit")) {
+					break;
+				}
+			}catch(Exception e) {
+				System.out.println(e.toString());
+			}
 		}
 	}
 
