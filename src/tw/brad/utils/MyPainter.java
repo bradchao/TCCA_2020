e4a7c80eebe332f7c27cbc27de55ccd159621b0c
user <user@PC323-96>
2020-07-26 16:28:58 +0800

    07260428

--- src/tw/brad/utils/MyPainter.java
+++ src/tw/brad/utils/MyPainter.java
@@ -4,10 +4,19 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
 
 public class MyPainter extends JPanel{
+	private MyMouseListener myMouseListener;
+	
+	public MyPainter() {
+		myMouseListener = new MyMouseListener();
+		addMouseListener(myMouseListener);
+		addMouseMotionListener(myMouseListener);
+	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -16,6 +25,20 @@ public class MyPainter extends JPanel{
 		g2d.setColor(Color.BLUE);
 		g2d.setStroke(new BasicStroke(4));
 		g2d.drawLine(0, 0, 100, 100);
+	}
+	
+	private class MyMouseListener extends MouseAdapter {
+		@Override
+		public void mousePressed(MouseEvent e) {
+			super.mousePressed(e);
+			System.out.println("Press: " + e.getX() + "," + e.getY());
+		}
+		
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			super.mouseDragged(e);
+			System.out.println("Drag: " + e.getX() + "," + e.getY());
+		}
 	}
 
 }
