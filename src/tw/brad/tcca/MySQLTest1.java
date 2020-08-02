6f0c4d685e1287bfb19c4bdd8dd9845ea604d229
user <user@PC323-96>
2020-08-02 14:29:17 +0800

    08021429

--- src/tw/brad/tcca/MySQLTest1.java
+++ src/tw/brad/tcca/MySQLTest1.java
@@ -3,6 +3,7 @@ package tw.brad.tcca;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class MySQLTest1 {
 
@@ -17,8 +18,21 @@ public class MySQLTest1 {
 		try {
 			// jdbc:sqlserver://<server>:<port>;databaseName=AdventureWorks;user=<user>;password=<password>
 			// jdbc:mysql://localhost:3306/GUESTBOOK?user=caterpillar&password=123456
+//			Connection conn = DriverManager.getConnection(
+//					"jdbc:mysql://localhost:8889/tcca?user=root&password=root&serverTimezone=Asia/Taipei");
+			
+//			Connection conn = DriverManager.getConnection(
+//					"jdbc:mysql://localhost:8889/tcca?serverTimezone=Asia/Taipei",
+//					"root","root");
+			
+			Properties prop = new Properties();
+			prop.put("user", "root");
+			prop.put("password", "root");
+			prop.put("serverTimezone", "Asia/Taipei");
 			Connection conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:8889/tcca?user=root&password=root&serverTimezone=Asia/Taipei");
+					"jdbc:mysql://localhost:8889/tcca",prop);
+			
+			
 			System.out.println("OK");
 		} catch (SQLException e) {
 			System.out.println(e.toString());
